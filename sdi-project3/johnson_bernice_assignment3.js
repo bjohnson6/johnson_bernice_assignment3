80452c96dd7f9307c735612af7756d95861dd20f
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-15 02:14:49 -0400

    testing

--- sdi-project3/johnson_bernice_assignment3.js
+++ sdi-project3/johnson_bernice_assignment3.js
@@ -3,6 +3,14 @@
 //Deliverable 3
 //Child Hood memories at my Grammys house.
 
-alert("Childhood memories with Grammy!");
+alert("Childhood memories with Grammy!"); 
+
+// stone house, fireplace, sidewalks, downingtown pa, big trees in the yard.
+//sell koolaid to the truck drivers and neighbors in the backyard.
+//icecream truck, musical sound of it coming and icecream, white truck with a big icecream on top.
+//freezer in sunroom filled with peanut butter tastykakes, creme soda in kitchen pantry(a-treat).
+//summer time, hot outside, go to five and dime store and buy penny candy.
+// Me and my sisters and Grammy. Amy and Cindy. Our ages, names.
+
 
 
