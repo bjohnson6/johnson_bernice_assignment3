22bea2a3848ae72e08f93a82ab7b8ecd4e0c1414
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-16 01:34:43 -0400

    Added my global variables and comments to them.

--- sdi-project3/johnson_bernice_assignment3.js
+++ sdi-project3/johnson_bernice_assignment3.js
@@ -6,11 +6,27 @@
 alert("Childhood memories with Grammy!"); 
 
 // stone house, fireplace, sidewalks, downingtown pa, big trees in the yard.
-//sell koolaid to the truck drivers and neighbors in the backyard.
+//sell koolaid to the truck drivers and neighbors in the backyard. (nested conditional, buy candy with money?)
 //icecream truck, musical sound of it coming and icecream, white truck with a big icecream on top.
 //freezer in sunroom filled with peanut butter tastykakes, creme soda in kitchen pantry(a-treat).
-//summer time, hot outside, go to five and dime store and buy penny candy.
+//summer time, hot outside, temperature, go to five and dime store and buy penny candy. Woodys store.
 // Me and my sisters and Grammy. Amy and Cindy. Our ages, names.
 
+
+//Global variables //need to have a string, number, boolean, array
+
+var         gMom    =  "Grammy";   //string
+var       summer    =   true;      //boolean
+var        store    =  "Woodys";   //string
+var         kAid    =  "koolaid";  //string
+var   pennyCandy    =   ["candy fish", "now and laters", "tootsy rolls" ]; //array
+var    candyCost    =   1;    //number
+var       iTruck    =   "icecream truck";   //string
+var           g1    =   "Bernice";   //string
+var           g2    =   "Cindy";     //string
+var           g2    =   "Amy";       //string
+var       iCream    =   "icecream";  //string
+var         cake    =   "peanut butter tastykakes";   //string
+var        drink    =   "creme soda";   //string
 
 
